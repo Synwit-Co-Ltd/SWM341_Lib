c0954abc8c09199be4d6f23c99a5f72f158fad35
XIVN1987 <XIVN1987@163.com>
2023-03-15 18:44:59 +0800

    定义 GPIO_INIT

--- SWM341_StdPeriph_Driver/APP/main.c
+++ SWM341_StdPeriph_Driver/APP/main.c
@@ -10,7 +10,9 @@ int main(void)
 	
 	SerialInit();
 	
-	GPIO_Init(GPIOA, PIN5, 1, 0, 0, 0);				//GPIOA.5配置为输出引脚，推挽输出
+	GPIO_Init(GPIOA, PIN5, 1, 0, 0, 0);			//GPIOA.5配置为输出引脚，推挽输出
+	
+	GPIO_INIT(GPIOA, PIN5, GPIO_OUTPUT);		//同上，另一种可读性更好的写法
 	
  	while(1==1)
  	{
