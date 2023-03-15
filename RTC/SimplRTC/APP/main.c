0d1aa236e6ed5b644d46419d42cb2e089b142cd6
XIVN1987 <XIVN1987@163.com>
2023-03-15 17:26:14 +0800

    RTC interrupt simplify

--- RTC/SimplRTC/APP/main.c
+++ RTC/SimplRTC/APP/main.c
@@ -32,9 +32,9 @@ void RTC_Handler(void)
 {
 	RTC_DateTime dateTime;
 	
-	if(RTC_IntSecondStat(RTC))
+	if(RTC_INTStat(RTC, RTC_IT_SECOND))
 	{
-		RTC_IntSecondClr(RTC);
+		RTC_INTClr(RTC, RTC_IT_SECOND);
 		
 		RTC_GetDateTime(RTC, &dateTime);
 		printf("Now Time: %02d : %02d\r\n", dateTime.Minute, dateTime.Second);
